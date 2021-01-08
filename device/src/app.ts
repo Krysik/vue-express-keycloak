471e7ff9fd8e93d5614a8324e92790e15d082776
Marcin Krysiński <marcin.krysik11@gmail.com>
2021-01-09 00:21:13 +0100

    GET endpoint for devices

--- device/src/app.ts
+++ device/src/app.ts
@@ -1,10 +1,24 @@
 import express from "express";
 import { config } from "dotenv";
+import fs from "fs";
+
 config();
 
 const app = express();
 
 app.use(express.json());
+
+app.get("/api/devices", (req, res) => {
+  fs.readFile("db.json", "utf8", (err, devices) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(500)
+        .json({ ok: false, message: "internal server error" });
+    }
+    return res.json(JSON.parse(devices));
+  });
+});
 
 const PORT = process.env.PORT || 4000;
 
